d3c7f9f4d1fc4bbbab6674f7948d358ba5a16a6d
RS_master <randomx46@gmail.com>
2024-05-04 18:26:22 +0300

    Access to postgreSQL from Django was set up

--- eduphoto/eduphoto/settings.py
+++ eduphoto/eduphoto/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = 'eduphoto.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "eduphoto"
+        }
     }
 }
 
