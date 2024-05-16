2f8b9eea8bf5bde97f61d49b300f99fce1834161
RS_master <randomx46@gmail.com>
2024-05-16 22:17:11 +0300

    Created our own piece of website
    Created new application, added custom urls with editable message.
    For now, the same thing is written on both urls.

---
+++ eduphoto/tasklist/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+
+def index(request):
+    context = {}
+    return render(
+        request,                   # Запрос
+	    'tasklist/index.html',     # путь к шаблону
+        context                    # подстановки
+    )
