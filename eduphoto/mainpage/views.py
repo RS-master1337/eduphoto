94183312710e1de72cbf01af54838a4e2767a5ea
RS_master <randomx46@gmail.com>
2024-05-28 21:33:54 +0300

    Настроил выводилку описания даты плана Если дата совпадает с введенной мной, вывожу "сегодня". Если введенная мной дата совпадает с моей -1, вывожу "вчера". Если ни с чем не совпадает, вывожу "когда-нибудь потом"

--- eduphoto/mainpage/views.py
+++ eduphoto/mainpage/views.py
@@ -1,12 +1,28 @@
 # Create your views here.
 from datetime import datetime
 from django.shortcuts import render
-def index(request):
+
+def index(request): 
+    plan = 'когда-нибудь потом'
+    kogdadata = [27, 6, 2024]
+    segodnya = datetime.now()
+    segodnya = datetime
+    (
+        segodnya.year,
+        segodnya.month,
+        segodnya.day,
+    )
+    if kogdadata[0] == segodnya.day and kogdadata[1] == segodnya.month and kogdadata[2] == segodnya.year:
+        plan = 'сегодня'
+    elif kogdadata[0] == (segodnya.day +1) and kogdadata[1] == segodnya.month and kogdadata[2] == segodnya.year:
+        plan = 'завтра'
+    elif kogdadata[0] == (segodnya.day -1) and kogdadata[1] == segodnya.month and kogdadata[2] == segodnya.year:
+        plan = 'вчера'
     return render(
         request,                    # Запрос
 	    'mainpage/index.html',
         {
-            'kogda': datetime.now(),
+            'kogda': plan,
             'tasks':[ # Они будут получены из базы данных, а не вбиты в код вручную!
                 {
                     'description': 'Не опаздывать',
